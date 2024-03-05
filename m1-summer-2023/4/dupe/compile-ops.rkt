662f6505cdc5f63977baed5da28242c3cdedf0e4
pathmarkoak <omkarp321@gmail.com>
2024-03-05 13:07:21 -0500

    4 done

--- m1-summer-2023/4/dupe/compile-ops.rkt
+++ m1-summer-2023/4/dupe/compile-ops.rkt
@@ -16,11 +16,12 @@
           (Mov r9  (value->bits #t))
           (Cmove rax r9))]
     ['odd? ;; compares last bit to 1, if yes then true, otherwise false
-      (let (odd-true (gensym 'odd?))
+      (let (odd-true (gensym 'odd?)) 
            (odd-false (gensym 'odd?))
-        (seq (And rax (value->bits 1))
+        (seq 
+             (And rax (value->bits 1))
              (Cmp rax (value->bits 0))
-             (Je odd-true)
+             (Jne odd-true)
              (Mov rax (value->bits #f))
              (Jmp odd-false)
              (Label odd-true)
---
+++ m1-summer-2023/4/dupe/test.rkt
@@ -0,0 +1,6 @@
+#lang racket
+(provide (all-defined-out))
+(require "ast.rkt" "types.rkt" a86/ast)
+
+
+(bits->value 110)
