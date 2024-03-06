e42dfe36307f29e24846748880d666ecef7e9744
Omkar Pathak <omkarp321@gmail.com>
2024-03-05 23:46:43 -0500

    syncing

--- m1-summer-2023/2/instr.rkt
+++ m1-summer-2023/2/instr.rkt
@@ -9,9 +9,4 @@
     (Sub 'rax 'rbx)
     (Sal 'rax 1)
     (Add 'rax 1)
-    ;; Assume encoding of n1 in rax
-    ;; and encoding of n2 in rbx
-
-    ;; TODO: write instructions that produce
-    ;; encoding of (- n1 n2) in rax
     ))
