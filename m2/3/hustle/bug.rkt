22153670fb8995b78994d933078880501d5a33fd
pathmarkoak <omkarp321@gmail.com>
2024-04-17 15:57:49 -0400

    3

--- m2/3/hustle/bug.rkt
+++ m2/3/hustle/bug.rkt
@@ -5,6 +5,7 @@
 ;; but when compiled it should produce 2.
 ;; cons twice, find car but it should clobber 1
 
-(car (unbox (box (cons 1 2))))
 
+(let ([x (cons 1 2)]) (begin (let ([y (cons 3 2)]) (cons 1 2)) (let ([z (cons 4 2)]) (car x))))
 
+;;; (let ([x (cons 1 1)]) (let ([y (cons 3 2)]) (car x)))
